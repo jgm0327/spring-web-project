df1196d4b3694352e40e97d39fc87ac599166415
jgm0327 <gmjang6349@gmail.com>
2022-10-02 13:17:09 +0900

    #1 - controller 와 service 수정
    Service에서 bno 와 연관된 첨부 파일 목록들을 Controller 를 통해 클라이언트한테 보여준다.

--- ex05/src/main/java/org/zerock/service/BoardSerivceImpl.java
+++ ex05/src/main/java/org/zerock/service/BoardSerivceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.zerock.domain.BoardAttachVO;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
 import org.zerock.mapper.BoardAttachMapper;
@@ -72,6 +73,12 @@ public class BoardSerivceImpl implements BoardService{
 	public int getTotal(Criteria cri) {
 		log.info("get total count");
 		return mapper.getTotalCount(cri);
+	}
+	
+	@Override
+	public List<BoardAttachVO> getAttachList(Long bno){
+		log.info("get Attach List by bno " + bno);
+		return attachMapper.findByBno(bno);
 	}
 
 }
