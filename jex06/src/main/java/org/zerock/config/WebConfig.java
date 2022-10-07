f817034e5a8bcde23ede1c82f25f2e9b71194150
jgm0327 <gmjang6349@gmail.com>
2022-10-07 19:28:18 +0900

    #8 - 잘못된 파일
    다시 푸쉬

--- jex06/src/main/java/org/zerock/config/WebConfig.java
+++ jex06/src/main/java/org/zerock/config/WebConfig.java
@@ -1,1 +1,31 @@
+package org.zerock.config;
+
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
 	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {RootConfig.class, SecurityConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+	
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+	}
+}
